c83f33ae1388537ed730378e7f07c55798b3d668
constROD <rialcantara21@gmail.com>
2023-04-30 17:32:25 +0800

    chore: update commit lint and pre-commit

--- packages/api/src/lambda.ts
+++ packages/api/src/lambda.ts
@@ -2,5 +2,5 @@ import { Time } from '@lib/utils';
 import { ApiHandler } from 'sst/node/api';
 
 export const handler = ApiHandler(async () => ({
-  body: `Hi Nick. The zxczxcxzc is ${Time.now()} ❌`,
+  body: `Hi Nick. The current time is: ${Time.now()} ❌`,
 }));
