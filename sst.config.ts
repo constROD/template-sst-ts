acd46b27ba2014aa2f4f0f29729c4893c367c03e
Rodrick <rialcantara21@gmail.com>
2023-05-01 19:25:31 +0800

    fix: esbuild config

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,25 @@
 import { SSTConfig } from 'sst';
 import { API } from './stacks/api';
 
+const esbuild = {
+  target: 'node16',
+  bundle: true,
+  minify: false,
+  sourcemap: true,
+  external: [
+    'aws-sdk',
+
+    /* Packages below are packages unnecessarily required by Knex.js */
+    'mysql',
+    'mysql2',
+    'better-sqlite3',
+    'pg-query-stream',
+    'tedious',
+    'oracledb',
+    'sqlite3',
+  ],
+};
+
 export default {
   config(_input) {
     return {
@@ -9,6 +28,7 @@ export default {
     };
   },
   stacks(app) {
+    app.setDefaultFunctionProps({ nodejs: { esbuild } });
     app.stack(API);
   },
 } satisfies SSTConfig;
--- stacks/api.ts
+++ stacks/api.ts
@@ -1,78 +1,19 @@
 import { Api, type StackContext } from 'sst/constructs';
 
-const esbuild = {
-  target: 'node16',
-  bundle: true,
-  minify: false,
-  sourcemap: true,
-  external: [
-    'aws-sdk',
-
-    /* Packages below are packages unnecessarily required by Knex.js */
-    'mysql',
-    'mysql2',
-    'better-sqlite3',
-    'pg-query-stream',
-    'tedious',
-    'oracledb',
-    'sqlite3',
-  ],
-};
-
 export function API({ stack }: StackContext) {
   const api = new Api(stack, 'API', {
     routes: {
       /* Swagger Docs */
-      'GET /': {
-        function: {
-          handler: 'packages/swagger/src/functions/swagger.handler',
-          nodejs: { esbuild },
-        },
-      },
-      'GET /swagger.json': {
-        function: {
-          handler: 'packages/swagger/src/functions/swagger.handler',
-          nodejs: { esbuild },
-        },
-      },
+      'GET /': 'packages/swagger/src/functions/swagger.handler',
+      'GET /swagger.json': 'packages/swagger/src/functions/swagger.handler',
 
       /* Users */
-      'GET /users': {
-        function: {
-          handler: 'packages/users/src/functions/users-list.handler',
-          nodejs: { esbuild },
-        },
-      },
-      'POST /users': {
-        function: {
-          handler: 'packages/users/src/functions/users-create.handler',
-          nodejs: { esbuild },
-        },
-      },
-      'GET /users/{id}': {
-        function: {
-          handler: 'packages/users/src/functions/users-get.handler',
-          nodejs: { esbuild },
-        },
-      },
-      'PUT /users/{id}': {
-        function: {
-          handler: 'packages/users/src/functions/users-update.handler',
-          nodejs: { esbuild },
-        },
-      },
-      'DELETE /users/{id}': {
-        function: {
-          handler: 'packages/users/src/functions/users-delete.handler',
-          nodejs: { esbuild },
-        },
-      },
-      'DELETE /users/{id}/archive': {
-        function: {
-          handler: 'packages/users/src/functions/users-archive.handler',
-          nodejs: { esbuild },
-        },
-      },
+      'GET /users': 'packages/users/src/functions/users-list.handler',
+      'POST /users': 'packages/users/src/functions/users-create.handler',
+      'GET /users/{id}': 'packages/users/src/functions/users-get.handler',
+      'PUT /users/{id}': 'packages/users/src/functions/users-update.handler',
+      'DELETE /users/{id}': 'packages/users/src/functions/users-delete.handler',
+      'DELETE /users/{id}/archive': 'packages/users/src/functions/users-archive.handler',
     },
   });
   stack.addOutputs({
