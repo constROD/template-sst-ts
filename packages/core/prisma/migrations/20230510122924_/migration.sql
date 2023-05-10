a3c4793b517ad4f93d1e183980b6997bd2ec76a0
constROD <rialcantara21@gmail.com>
2023-05-10 20:30:27 +0800

    chore: update schema

--- packages/core/prisma/migrations/20230510122924_/migration.sql
+++ packages/core/prisma/migrations/20230510122924_/migration.sql
@@ -5,7 +5,7 @@ CREATE TABLE `persons` (
     `last_name` VARCHAR(255) NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `archived_at` DATETIME NULL,
+    `archived_at` DATETIME(3) NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
